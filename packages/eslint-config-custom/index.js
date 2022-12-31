448c6788ea50b98243de7f42318ab2f564cabc3f
siropaca <ryhshr3@gmail.com>
2022-12-31 23:15:37 +0900

    :wrench: eslint-plugin-unused-imports追加

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['next', 'turbo', 'prettier'],
-  plugins: ['import'],
+  plugins: ['import', 'unused-imports'],
+  extends: ['next', 'turbo', 'eslint:recommended', 'prettier'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
@@ -20,6 +20,11 @@ module.exports = {
         pathGroupsExcludedImportTypes: ['builtin'],
         alphabetize: { order: 'asc', caseInsensitive: true },
       },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
   },
 };
