5e408b00634f85c3770c921ff05db6946fdce575
siropaca <ryhshr3@gmail.com>
2023-01-01 03:30:01 +0900

    :tada: GraphQLでデータ取得まで

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,9 +1,42 @@
+import { gql, useQuery } from '@apollo/client';
 import { Button } from 'ui';
 
+interface MyQuery {
+  articles: {
+    id: string;
+    title: string;
+    body: string;
+  }[];
+}
+
 export default function Web() {
+  const { loading, error, data } = useQuery<MyQuery>(gql`
+    query {
+      articles {
+        id
+        title
+        body
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error: {JSON.stringify(error)}</p>;
+
   return (
     <div>
       <h1>Web</h1>
+
+      {data!.articles.map((article) => {
+        return (
+          <div key={article.id}>
+            <div>{article.title}</div>
+            <div>{article.body}</div>
+          </div>
+        );
+      })}
+
       <Button />
     </div>
   );
