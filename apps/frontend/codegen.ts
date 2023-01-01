ba08f5d754a1f76f627f87268ad9e947e7612d2b
siropaca <ryhshr3@gmail.com>
2023-01-01 11:27:30 +0900

    :recycle: dotenvで読み込むように変更

--- apps/frontend/codegen.ts
+++ apps/frontend/codegen.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
+
 import type { CodegenConfig } from '@graphql-codegen/cli';
+
+dotenv.config();
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'https://ap-northeast-1.cdn.hygraph.com/content/clcbwdybf3fuw01uhb2tnb5no/master',
+  schema: process.env.NEXT_PUBLIC_API_ENDPOINT,
   documents: 'src/**/*.tsx',
   generates: {
     'src/gql/': {
