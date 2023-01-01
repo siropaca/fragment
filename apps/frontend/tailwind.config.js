0c74add4961d475565359f5cb06e33834512beea
shirai <ryhshr3@gmail.com>
2023-01-01 19:12:33 +0900

    :wrench: tailwind.config.jsにscreensを追加

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
   theme: {
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '769px',
+      // => @media (min-width: 769px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
