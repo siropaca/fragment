3360673065093917342380c9b7dc7b25e2824989
siropaca <ryhshr3@gmail.com>
2023-01-01 15:57:56 +0900

    :sparkles: tags追加

--- apps/frontend/src/pages/articles/[articleId].tsx
+++ apps/frontend/src/pages/articles/[articleId].tsx
@@ -1,18 +1,58 @@
+import { useQuery } from '@apollo/client';
 import { GetServerSideProps, NextPage } from 'next';
+
+import { graphql } from '@/gql';
 
 interface Props {
   articleId: string;
 }
 
+const queryDocument = graphql(`
+  query Article($where: ArticleWhereUniqueInput!) {
+    article(where: $where) {
+      id
+      title
+      description
+      tags
+      articleNodes {
+        id
+        boby
+      }
+    }
+  }
+`);
+
 const ArticlesDetail: NextPage<Props> = (props) => {
-  return <div>{props.articleId}の詳細</div>;
+  const { loading, error, data } = useQuery(queryDocument, {
+    variables: {
+      where: {
+        id: props.articleId,
+      },
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error: {JSON.stringify(error)}</p>;
+
+  if (!data) return <p>No Data!</p>;
+
+  return (
+    <div>
+      <h1>{data.article?.title}</h1>
+      <h2>{data.article?.description}</h2>
+      <h2>{JSON.stringify(data.article?.tags)}</h2>
+      <hr />
+      {data.article?.articleNodes.map((node) => {
+        return <div key={node.id}>{node.boby}</div>;
+      })}
+    </div>
+  );
 };
 
 export default ArticlesDetail;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  console.log(context);
-
   return {
     props: {
       ...context.params,
