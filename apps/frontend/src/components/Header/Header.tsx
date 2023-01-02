ccd0c8be4f083426329ad39a7f3a41ba4e4adebc
siropaca <ryhshr3@gmail.com>
2023-01-02 20:42:30 +0900

    :art: Header修正

--- apps/frontend/src/components/Header/Header.tsx
+++ apps/frontend/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import process from 'process';
 
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -8,6 +8,57 @@ import Link from 'next/link';
 import { URL } from '@/lib/router';
 
 const HEADER_HEIGHT = '62px';
+
+const MENUS = [
+  {
+    a: {
+      href: URL.blog(),
+    },
+    img: {
+      src: '/texts/Blog.svg',
+      alt: 'Blog',
+      width: 49,
+      height: 20,
+    },
+  },
+  // TODO: ページを追加する
+  {
+    a: {
+      href: URL.root(),
+    },
+    img: {
+      src: '/texts/Tools.svg',
+      alt: 'Tools',
+      width: 60,
+      height: 18,
+    },
+  },
+  // TODO: ページを追加する
+  {
+    a: {
+      href: URL.root(),
+    },
+    img: {
+      src: '/texts/About.svg',
+      alt: 'About',
+      width: 71,
+      height: 18,
+    },
+  },
+  // TODO: メールアドレスを設定する
+  {
+    a: {
+      href: '/texts/Contact.svg',
+      rel: 'noreferrer',
+    },
+    img: {
+      src: '/texts/Contact.svg',
+      alt: 'Contact',
+      width: 89,
+      height: 18,
+    },
+  },
+];
 
 export const Header = (): JSX.Element => {
   const siteName = process.env.NEXT_PUBLIC_SITE_NAME;
@@ -29,65 +80,30 @@ export const Header = (): JSX.Element => {
           />
         </Link>
 
-        <div className='flex gap-x-6'>
-          <Link
-            href={URL.blog()}
-            className='hover:opacity-50'
-          >
-            <Image
-              src='/texts/Blog.svg'
-              alt='Blog'
-              width={49}
-              height={20}
-            />
-          </Link>
+        {/* TODO: 実装する */}
+        {/* ハンバーガーメニュー */}
+        <span className='lg:hidden'>
+          <FontAwesomeIcon
+            icon={faBars}
+            className='text-gray-600'
+            size='xl'
+          />
+        </span>
 
-          {/* TODO: ページを追加する */}
-          <Link
-            href={URL.root()}
-            className='hover:opacity-50'
-          >
-            <Image
-              src='/texts/Tools.svg'
-              alt='Blog'
-              width={60}
-              height={18}
-            />
-          </Link>
-
-          {/* TODO: ページを追加する */}
-          <Link
-            href={URL.root()}
-            className='hover:opacity-50'
-          >
-            <Image
-              src='/texts/About.svg'
-              alt='Blog'
-              width={71}
-              height={18}
-            />
-          </Link>
-
-          {/* TODO: メールアドレスを設定する */}
-          <a
-            href='mailto:info&#64;example.com'
-            target='_blank'
-            className='inline-flex items-end gap-x-2 hover:opacity-50'
-            rel='noreferrer'
-          >
-            <Image
-              src='/texts/Contact.svg'
-              alt='Blog'
-              width={89}
-              height={18}
-            />
-
-            <FontAwesomeIcon
-              icon={faArrowUpRightFromSquare}
-              className='text-gray-400'
-              size='2xs'
-            />
-          </a>
+        {/* PC用メニュー */}
+        <div className='hidden gap-x-6 lg:flex'>
+          {MENUS.map((menu, index) => {
+            return (
+              <Link
+                key={index}
+                {...menu.a}
+                className='hover:opacity-50'
+              >
+                {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                <Image {...menu.img} />
+              </Link>
+            );
+          })}
         </div>
       </div>
     </header>
