caa18d9810bc02bbe648d4d0e3efd7476728a7cc
siropaca <ryhshr3@gmail.com>
2023-01-04 22:58:13 +0900

    :+1: components整理

--- apps/frontend/src/components/Layout/ContentsLayout.tsx
+++ apps/frontend/src/components/Layout/ContentsLayout.tsx
@@ -1,6 +1,5 @@
-import { Footer } from '@/components/Footer';
-import { HeadProps, Head } from '@/components/Head';
-import { Header } from '@/components/Header';
+import { Footer, Header } from '@/components/Navigation';
+import { HeadProps, Head } from '@/components/Utils';
 
 export const CONTENTS_MAX_WIDTH = '1192px';
 
