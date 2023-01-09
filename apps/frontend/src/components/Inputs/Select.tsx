105e5ff7666a3ea467ffbed80ff42ac95d3d429b
siropaca <ryhshr3@gmail.com>
2023-01-09 14:25:30 +0900

    :sparkles: Select追加

---
+++ apps/frontend/src/components/Inputs/Select.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx';
+import React from 'react';
+
+interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  options: { label: string; value: string }[];
+}
+
+export const Select = React.forwardRef<HTMLSelectElement, Props>(
+  ({ options, ...selectProps }: Props, ref) => {
+    return (
+      <select
+        ref={ref}
+        {...selectProps}
+        className={clsx(
+          'cursor-pointer rounded-lg border border-gray-200 bg-transparent px-3 py-2 focus:outline-none dark:border-gray-600',
+          selectProps.className,
+        )}
+      >
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-argument */}
+        {options.map((option) => {
+          return (
+            <option
+              key={option.value}
+              value={option.value}
+            >
+              {option.label}
+            </option>
+          );
+        })}
+      </select>
+    );
+  },
+);
+
+Select.displayName = 'Select';
