b601e64cd795967928cc971186497a052f57191b
siropaca <ryhshr3@gmail.com>
2023-01-09 22:39:05 +0900

    :art: 404スタイル修正

--- apps/frontend/src/pages/404.tsx
+++ apps/frontend/src/pages/404.tsx
@@ -12,13 +12,15 @@ const Custom404Page: NextPage = () => {
       pageType='article'
       pageUrl={PagePath.notfound(true)}
     >
-      <div className='min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8'>
+      <div className='min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8'>
         <div className='mx-auto max-w-max'>
           <main className='sm:flex'>
-            <p className='text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl'>404</p>
+            <p className='text-4xl font-bold tracking-tight text-cyan-600 sm:text-5xl'>404</p>
             <div className='sm:ml-6'>
               <div className='sm:border-l sm:border-gray-200 sm:pl-6'>
-                <h1 className='text-4xl font-bold tracking-tight sm:text-5xl'>Page not found</h1>
+                <h1 className='mb-4 text-4xl font-bold tracking-tight sm:text-5xl'>
+                  Page not found
+                </h1>
 
                 <p className='mt-1 text-base text-gray-500'>
                   Please check the URL in the address bar and try again.
@@ -30,7 +32,7 @@ const Custom404Page: NextPage = () => {
           <div className='mt-14 text-center'>
             <Link
               href={PagePath.root()}
-              className='text-base font-medium text-indigo-600 hover:text-indigo-500'
+              className='font-medium text-cyan-600 hover:opacity-50'
             >
               Go back home
               <span> &rarr;</span>
